d5e89f084a98f8f776a1986899fe6b30f4dca905
RayyaneElmalhouni <elmalhounirayyane@gmail.com>
2022-03-01 06:11:16 +0100

    set the components

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,15 @@
 <script>
-	export let name;
+	import Search from './components/Search.svelte';
+	import Show from './components/Show.svelte';
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Breaking Bad</h1>
+	<h2>Actor Searcher</h2>
+	<Search />
+	<Show />
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+	
 </style>
